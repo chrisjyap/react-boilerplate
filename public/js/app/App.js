974a0de6f3930a653803c4af01f5307da359b9c4
chrisjyap <chrisjyap@gmail.com>
2016-05-30 11:48:19 -0700

    creating redux store

--- public/js/app/App.js
+++ public/js/app/App.js
@@ -1,4 +1,12 @@
-import React from 'react';
+import React                  from 'react';
+import { Provider }           from 'react-redux';
+import {
+        createStore,
+        applyMiddleware,
+        combineReducers
+}                             from 'redux';
+import thunk                  from 'redux-thunk';
+import reducers               from '../reducers';
 
 import styles from '../../sass/main.scss';
 
@@ -6,7 +14,21 @@ export default class App extends React.Component {
   constructor (props) {
     super(props);
   }
+
+  componentWillMount () {
+    const allReducers = combineReducers(reducers);
+    const store = createStore(
+      allReducers,
+      applyMiddleware(thunk)
+    );
+    this.setState({
+      store
+    });
+  }
+
   render () {
+    console.log('render', this.state.store);
+    const { store } = this.state;
     return (
       <div className={styles.mainApp}>
         First app in HMR
